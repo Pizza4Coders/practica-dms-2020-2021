554216fb5cd6e8556ca166d4c560f11b2b950954
cgp1003 <cgp1003@alu.ubu.es>
2020-12-15 00:19:16 +0100

    Sensorsservice doc

--- components/dms2021client/dms2021client/data/rest/sensorsservice.py
+++ components/dms2021client/dms2021client/data/rest/sensorsservice.py
@@ -48,3 +48,76 @@ class SensorsService():
             return False
         except ConnectionRefusedError:
             return False
+    
+    def get_all_rules(self):
+        """ Get the list of rules.
+        ---
+        Returns:
+            A dictionary with the list of rules, where each has rule_name, type,
+            data and frequency
+        Throws:
+            - HTTPException: On an unhandled 500 error.
+        """
+
+
+    def get_rule(self, rulename: str):
+        """ Get the specified rule.
+        ---
+        Parameters:
+            - rulename: The rule name string.
+        Returns:
+            A dictionary with a rule, where it has rule_name, type,
+            data and frequency
+        Throws:
+            - BadRequestError: If the request is malformed.
+            - NotFoundError: If the rule does not exist.
+            - HTTPException: On an unhandled 500 error.
+        """
+
+    def create_rule(self, rulename: str, ruletype: str, ruleargs: str, frequency: int):
+        """ Create a new rule.
+        ---
+        Parameters:
+            - rulename: The rule name string.
+            - ruletype: The type of the rule string. (text: command, file)
+            - ruleargs: A command or a file path.
+            - frequency (seconds): 0 if it does not execute automatically.
+        Throws:
+            - BadRequestError: If the request is malformed.
+            - ConflictError: If the rule already exists.
+            - HTTPException: On an unhandled 500 error.
+        """
+
+    def delete_rule(self, rulename: str):
+        """ Removes a specified rule.
+        ---
+        Parameters:
+            - rulename: The rule name string.
+        Throws:
+            - BadRequestError: If the request is malformed.
+            - NotFoundError: If the rule does not exist.
+            - HTTPException: On an unhandled 500 error.
+        """
+
+    def run_rule(self, rulename: str):
+        """ Runs a specified rule.
+        ---
+        Parameters:
+            - rulename: The rule name string.
+        Returns:
+            A dictionary with the output (str).
+        Throws:
+            - BadRequestError: If the request is malformed.
+            - NotFoundError: If the rule does not exist.
+            - HTTPException: On an unhandled 500 error or if the rule failed.
+        """
+
+    def get_log(self):
+        """ Get the log.
+        ---
+        Returns:
+            A dictionary with the list of results, where it has rule_name, time and
+            result.
+        Throws:
+            - HTTPException: On an unhandled 500 error.
+        """
