f16249e7b956494399ca048f6fbed06ee38e8f26
Pablo Fdez <pablofdezbravo@gmail.com>
2020-12-18 04:44:31 +0100

    Changed Menus

--- components/dms2021client/dms2021client/presentation/menus/menu.py
+++ components/dms2021client/dms2021client/presentation/menus/menu.py
@@ -1,41 +1,78 @@
-""" Menu class module.
+""" Module containing the dms2021client.presentation.menus.Menu class
 """
 
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Callable
 
 class Menu(ABC):
-    """Class menu.
+    """ Class responsible of creating menus.
     """
+
     _title: str = ""
     _items: List[str] = []
+    _opt_functions: List[Callable] = []
+
+    @abstractmethod
+    def set_title(self, title: str) -> None:
+        """ Sets the menu title.
+
+        Subclasses are expected to override this method and provide their own
+        implementation.
+        ---
+        Parameters:
+            - title: A string with the title that will be displayed in the menu.
+        """
+        return
+
+    @abstractmethod
+    def set_items(self, items: List[str]) -> None:
+        """ Sets the menu items.
+
+        Subclasses are expected to override this method and provide their own
+        implementation.
+        ---
+        Parameters:
+            - items: A list with the strings that will display the menu options.
+        """
+        return
+
+    @abstractmethod
+    def set_opt_fuctions(self, functions: List[Callable]) -> None:
+        """ Sets the functions that will be executed when you select one option.
+
+        Subclasses are expected to override this method and provide their own
+        implementation.
+        ---
+        Parameters:
+            - functions: A list with the functions that will be called when
+            a menu option is selected.
+        """
+        return
 
     @abstractmethod
     def _draw_title(self) -> None:
-        """Draws the menu title.
+        """ Display the menu title.
+
+        Subclasses are expected to override this method and provide their own
+        implementation.
         """
         return
 
     @abstractmethod
     def _draw_items(self) -> None:
-        """Draws the menu items.
+        """ Display the menu items.
+
+        Subclasses are expected to override this method and provide their own
+        implementation.
         """
         return
 
     @abstractmethod
-    def _draw_menu(self) -> int:
-        """Draws the menu.
-        """
-        return 0
+    def show_options(self) -> None:
+        """ Display the menu and controls the actions to be executed when one option
+        have been selected.
 
-    @abstractmethod
-    def _set_title(self) -> None:
-        """Sets the menu title.
-        """
-        return
-
-    @abstractmethod
-    def _set_items(self) -> None:
-        """Sets the menu items
+        Subclasses are expected to override this method and provide their own
+        implementation.
         """
         return
--- components/dms2021client/dms2021client/presentation/menus/orderedmenu.py
+++ components/dms2021client/dms2021client/presentation/menus/orderedmenu.py
@@ -1,41 +1,63 @@
-"""Ordered Menu Class
+"""OrderedMenu class module.
 """
 
-from typing import List
+from typing import List, Callable
 from dms2021client.presentation.menus import Menu
 
 class OrderedMenu(Menu):
     """Class Ordered Menu
     """
+
     _ordered_title: str = ""
     _ordered_items: List[str] = []
+    _ordered_opt_functions: List[Callable] = []
+
+    def set_title(self, title: str) -> None:
+        """ Sets the menu title.
+        ---
+        Parameters:
+            - title: A string with the title that will be displayed in the menu.
+        """
+        self._ordered_title = title
+
+    def set_items(self, items: List[str]) -> None:
+        """ Sets the menu items.
+        ---
+        Parameters:
+            - items: A list with the strings that will display the menu options.
+        """
+        self._ordered_items = items
+
+    def set_opt_fuctions(self, functions: List[Callable]) -> None:
+        """ Sets the function that will be executed when you select one option.
+        Parameters:
+            - functions: A list with the functions that will be called when
+            a menu option is selected.
+        """
+        self._ordered_opt_functions = functions
 
     def _draw_title(self) -> None:
-        """Draws the menu title.
+        """ Display the menu title.
         """
         print("-"*20 + self._ordered_title + "-"*20 + "\n")
 
     def _draw_items(self) -> None:
-        """Draws the menu items.
+        """ Display the menu items.
         """
         for i, item in enumerate(self._ordered_items, 1):
             print(str(i) + ". " + item)
-
-    def _draw_menu(self) -> int:
-        """Draws the menu.
-        """
-        self._draw_title()
-        self._draw_items()
         print("-"*(40+len(self._ordered_title)))
-        selected_opt = int(input("Select an option from above: "))
-        return selected_opt
 
-    def _set_title(self) -> None:
-        """Sets the menu title.
+    def show_options(self) -> None:
+        """ Display the menu and controls the actions to be executed when one option
+        have been selected.
         """
-        self._ordered_title = ""
-
-    def _set_items(self) -> None:
-        """Sets the menu items.
-        """
-        self._ordered_items = []
+        selected_opt: int = 0
+        while 0 > selected_opt > len(self._ordered_opt_functions):
+            try:
+                self._draw_title()
+                self._draw_items()
+                selected_opt = int(input("Selecciona una opción: "))
+            except ValueError:
+                pass
+        self._ordered_opt_functions[selected_opt - 1]()
