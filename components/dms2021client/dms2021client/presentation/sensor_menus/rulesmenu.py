23352176aa56fe2234da9b062baa7fc9908e6bb3
Pablo Fdez <pablofdezbravo@gmail.com>
2021-01-17 18:09:50 +0100

    Fixed RulesMenu exceptions

--- components/dms2021client/dms2021client/presentation/sensor_menus/rulesmenu.py
+++ components/dms2021client/dms2021client/presentation/sensor_menus/rulesmenu.py
@@ -3,7 +3,7 @@
 
 from typing import List, Callable
 from http.client import HTTPException
-from dms2021client.data.rest.exc import BadRequestError, ConflictError, NotFoundError
+from dms2021client.data.rest.exc import BadRequestError, NotFoundError
 from dms2021client.data.rest.exc import UnauthorizedError
 from dms2021client.data.rest import AuthService, SensorsService
 from dms2021client.presentation.sensor_menus.addrulesmenu import AddRulesMenu
@@ -45,23 +45,12 @@ class RulesMenu(OrderedMenu):
             - items: A list with the strings that will display the menu options.
         """
         items: List[str] = []
-        try:
-            if self.__authservice.has_right(self.__username, "AdminRules"):
-                items += ["Ver reglas", "Añadir regla", "Eliminar regla"]
-                if self.__authservice.has_right(self.__username, "ViewReports"):
-                    items.append("Ejecutar regla")
+        if self.__authservice.has_right(self.__username, "AdminRules"):
+            items += ["Ver reglas", "Añadir regla", "Eliminar regla"]
             if self.__authservice.has_right(self.__username, "ViewReports"):
-                items.append("Ver historial de ejecución")
-        except BadRequestError:
-            print("Se han introducido parámetros incorrectos.")
-        except UnauthorizedError:
-            print("Usted no tiene permisos para realizar esta acción.")
-        except NotFoundError:
-            print("No existe una regla con ese nombre.")
-        except ConflictError:
-            print("Ya existe una regla con ese nombre.")
-        except HTTPException:
-            print("Ha ocurrido un error inesperado.")
+                items.append("Ejecutar regla")
+        if self.__authservice.has_right(self.__username, "ViewReports"):
+            items.append("Ver historial de ejecución")
         self._ordered_items = items
 
     def set_opt_fuctions(self) -> None:
@@ -71,61 +60,78 @@ class RulesMenu(OrderedMenu):
             a menu option is selected.
         """
         functions: List[Callable] = []
-        try:
-            if self.__authservice.has_right(self.__username, "AdminRules"):
-                functions += [self.get_rules,
-                    AddRulesMenu(self.__session_token, self.__username,
-                    self.__authservice, self.__sensorservice).show_options,
-                    self.remove_rules]
-                if self.__authservice.has_right(self.__username, "ViewReports"):
-                    functions.append(self.run_rule)
+        if self.__authservice.has_right(self.__username, "AdminRules"):
+            functions += [self.get_rules,
+                AddRulesMenu(self.__session_token, self.__username,
+                self.__authservice, self.__sensorservice).show_options,
+                self.remove_rules]
             if self.__authservice.has_right(self.__username, "ViewReports"):
-                functions.append(self.get_log)
-        except BadRequestError:
-            print("Se han introducido parámetros incorrectos.")
-        except UnauthorizedError:
-            print("Usted no tiene permisos para realizar esta acción.")
-        except NotFoundError:
-            print("No existe una regla con ese nombre.")
-        except ConflictError:
-            print("Ya existe una regla con ese nombre.")
-        except HTTPException:
-            print("Ha ocurrido un error inesperado.")
+                functions.append(self.run_rule)
+        if self.__authservice.has_right(self.__username, "ViewReports"):
+            functions.append(self.get_log)
         self._ordered_opt_functions = functions
 
     def get_rules(self) -> None:
         """ Gets the list of rules.
         """
         print("-"*20 + "VER REGLAS" + "-"*20 + "\n")
-        result: List[dict] = self.__sensorservice.get_all_rules(self.__username)
-        for rule in result:
-            for k in rule:
-                print("[" + k.upper() + "] -> " + str(rule[k]))
-            print("-"*50)
+        try:
+            result: List[dict] = self.__sensorservice.get_all_rules(self.__username)
+            for rule in result:
+                for k in rule:
+                    print("[" + k.upper() + "] -> " + str(rule[k]))
+                print("-"*50)
+        except UnauthorizedError:
+            print("Usted no tiene permisos para realizar esta acción.")
+        except HTTPException:
+            print("Ha ocurrido un error inesperado.")
 
     def remove_rules(self) -> None:
         """ Removes a specified rule.
         """
-        print("-"*20 + "ELIMINAR REGLA" + "-"*20 + "\n")
-        rulename: str = input("Introduzca el nombre de la regla: ")
-        self.__sensorservice.delete_rule(rulename, self.__username)
-        print("\n La regla " + rulename + " ha sido eliminada correctamente.")
+        try:
+            print("-"*20 + "ELIMINAR REGLA" + "-"*20 + "\n")
+            rulename: str = input("Introduzca el nombre de la regla: ")
+            self.__sensorservice.delete_rule(rulename, self.__username)
+            print("\n La regla " + rulename + " ha sido eliminada correctamente.")
+        except BadRequestError:
+            print("Se han introducido parámetros incorrectos.")
+        except UnauthorizedError:
+            print("Usted no tiene permisos para realizar esta acción.")
+        except NotFoundError:
+            print("No existe una regla con ese nombre.")
+        except HTTPException:
+            print("Ha ocurrido un error inesperado.")
 
     def run_rule(self) -> None:
         """ Runs a specified rule.
         """
-        print("-"*20 + "EJECUTAR REGLA" + "-"*20 + "\n")
-        rulename: str = input("Introduzca el nombre de la regla: ")
-        result: dict = self.__sensorservice.run_rule(rulename, self.__username)
-        print("Resultado de la ejecución: ")
-        print(result["result"])
+        try:
+            print("-"*20 + "EJECUTAR REGLA" + "-"*20 + "\n")
+            rulename: str = input("Introduzca el nombre de la regla: ")
+            result: dict = self.__sensorservice.run_rule(rulename, self.__username)
+            print("Resultado de la ejecución: ")
+            print(result["result"])
+        except BadRequestError:
+            print("Se han introducido parámetros incorrectos.")
+        except UnauthorizedError:
+            print("Usted no tiene permisos para realizar esta acción.")
+        except NotFoundError:
+            print("No existe una regla con ese nombre.")
+        except HTTPException:
+            print("Ha ocurrido un error inesperado.")
 
     def get_log(self) -> None:
         """ Gets the log.
         """
-        print("-"*20 + "VER HISTORIAL DE EJECUCIÓN" + "-"*20 + "\n")
-        result: List[dict] = self.__sensorservice.get_log(self.__username)
-        for rule in result:
-            for k in rule:
-                print("[" + k.upper() + "] -> " + str(rule[k]))
-            print("-"*66)
+        try:
+            print("-"*20 + "VER HISTORIAL DE EJECUCIÓN" + "-"*20 + "\n")
+            result: List[dict] = self.__sensorservice.get_log(self.__username)
+            for rule in result:
+                for k in rule:
+                    print("[" + k.upper() + "] -> " + str(rule[k]))
+                print("-"*66)
+        except UnauthorizedError:
+            print("Usted no tiene permisos para realizar esta acción.")
+        except HTTPException:
+            print("Ha ocurrido un error inesperado.")
