39598a1a1909a8028b6586cfede38fd0d66519df
cgp1003 <cgp1003@alu.ubu.es>
2021-01-17 03:45:43 +0100

    Fixed typos and unused imports

--- components/dms2021sensor/dms2021sensor/logic/rulerunners/cpurulerunner.py
+++ components/dms2021sensor/dms2021sensor/logic/rulerunners/cpurulerunner.py
@@ -1,14 +1,13 @@
 """ Base rule runner module
 """
 
-from os.path import isfile
 from dms2021sensor.data.db.results import Rule
 from dms2021sensor.logic.rulerunners.baserulerunner import BaseRuleRunner
 from dms2021sensor.logic.rulerunners.exc import RuleRunError
 import psutil #type: ignore
 
 class CPURuleRunner(BaseRuleRunner):
-    """ Rule runner when type is "file".
+    """ Rule runner when type is "cpu".
     """
     @staticmethod
     def run_rule(rule: Rule) -> str:
@@ -22,9 +21,8 @@ class CPURuleRunner(BaseRuleRunner):
         try:
             if rule.data == "all":
                 return psutil.cpu_percent(0.5)
-            else:
-                num_core = int(rule.data)
-                cpu_loads = psutil.cpu_percent(0.5, True)
-                return cpu_loads[num_core]
+            num_core = int(rule.data)
+            cpu_loads = psutil.cpu_percent(0.5, True)
+            return cpu_loads[num_core]
         except Exception as ex:
             raise RuleRunError from ex
