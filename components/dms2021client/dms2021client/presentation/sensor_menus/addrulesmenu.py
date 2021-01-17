13fcf58bf21c03e8918555ce98f78d3d70b74301
Pablo Fdez <pablofdezbravo@gmail.com>
2021-01-17 18:13:44 +0100

    Adapted AddRulesMenu

--- components/dms2021client/dms2021client/presentation/sensor_menus/addrulesmenu.py
+++ components/dms2021client/dms2021client/presentation/sensor_menus/addrulesmenu.py
@@ -28,44 +28,59 @@ class AddRulesMenu(OrderedMenu):
         self.__authservice: AuthService = authservice
         self.__sensorservice: SensorsService = sensorservice
 
-    def show_options(self):
-        """ Shows options to modify rules or view reports, depends on the rights
-        the user has.
+    def set_title(self) -> None:
+        """ Sets the menu title.
+        ---
+        Parameters:
+            - title: A string with the title that will be displayed in the menu.
         """
-        while not self._returning:
-            options: List[str] = []
-            functions: List[Callable] = []
+        self._ordered_title = "SELECCIONE EL TIPO DE REGLA A AÑADIR"
 
-            self.set_title("SELECCIONE EL TIPO DE REGLA A AÑADIR")
-            if self.__authservice.has_right(self.__username, "AdminRules"):
-                options += ["Fichero. Comprueba la existencia de un fichero",
-                "Comando. Ejecuta un comando Linux", "CPU. Comprueba el uso de la cpu"]
-                functions += [self.add_file_rule, self.add_command_rule, self.add_cpu_rule]
-            super().set_items(options)
-            super().set_opt_fuctions(functions)
-            try:
-                super().show_options()
-            except BadRequestError:
-                print("Se han introducido parámetros incorrectos.")
-            except UnauthorizedError:
-                print("Usted no tiene permisos para realizar esta acción.")
-            except ConflictError:
-                print("Ya existe una regla con ese nombre.")
-            except HTTPException:
-                print("Ha ocurrido un error inesperado.")
+    def set_items(self) -> None:
+        """ Sets the menu items.
+        ---
+        Parameters:
+            - items: A list with the strings that will display the menu options.
+        """
+        items: List[str] = []
+        if self.__authservice.has_right(self.__username, "AdminRules"):
+            items += ["Fichero. Comprueba la existencia de un fichero",
+            "Comando. Ejecuta un comando Linux", "CPU. Comprueba el uso de la cpu"]
+        self._ordered_items = items
+
+    def set_opt_fuctions(self) -> None:
+        """ Sets the function that will be executed when you select one option.
+        Parameters:
+            - functions: A list with the functions that will be called when
+            a menu option is selected.
+        """
+        functions: List[Callable] = []
+        if self.__authservice.has_right(self.__username, "AdminRules"):
+            functions += [self.add_file_rule, self.add_command_rule, self.add_cpu_rule]
+        self._ordered_opt_functions = functions
 
     def add_rules(self, rulename: str, ruletype: str, ruleargs: str) -> None:
         """ Creates a new rule.
         """
         frequency: int
-        while True:
-            try:
-                frequency = int(input("Introduzca la frecuencia de ejecución en segundos: "))
-                break
-            except ValueError:
-                print("Valor incorrecto.")
-        self.__sensorservice.create_rule(rulename, ruletype, ruleargs, frequency, self.__username)
-        print("\n La regla " + rulename + " ha sido creada correctamente.")
+        try:
+            while True:
+                try:
+                    frequency = int(input("Introduzca la frecuencia de ejecución en segundos: "))
+                    break
+                except ValueError:
+                    print("Valor incorrecto.")
+            self.__sensorservice.create_rule(rulename, ruletype,
+                ruleargs,frequency, self.__username)
+            print("\n La regla " + rulename + " ha sido creada correctamente.")
+        except BadRequestError:
+            print("Se han introducido parámetros incorrectos.")
+        except UnauthorizedError:
+            print("Usted no tiene permisos para realizar esta acción.")
+        except ConflictError:
+            print("Ya existe una regla con ese nombre.")
+        except HTTPException:
+            print("Ha ocurrido un error inesperado.")
 
     def add_file_rule(self) -> None:
         """ Creates a new file checking rule.
